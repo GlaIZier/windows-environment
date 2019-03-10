b482969f03ecdfe38194b169aec517b3fde5d2a3
GlaIZier <kh.mv@bk.ru>
2019-03-10 13:33:21 +0300

    .travis.yml has been edited

--- folders.bat
+++ folders.bat
@@ -1,4 +1,13 @@
-set folders = "D:/google-drive D:/movies D:/tv-series D:/downloads D:/games"
+set disk = %1
+if [%disk%] == [] (
+    set disk = "D"
+)
+
+echo Disk is set to %disk%
+
+set folders = "%disk%:\google-drive %disk%:\movies %disk%:\tv-series %disk%:\downloads %disk%:\games"
+
+echo Folders to be created %folders%
 
 for %%f in (%folders%) do (
     IF EXIST %f% (
--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
-choco install packages.config -y --debug
+:: installs to a default chocolatey directory https://chocolatey.org/docs/default-chocolatey-install-reasoning
+choco install packages.config -y
 
 echo Applications have been installed
 
-call folders.bat
+call folders.bat %1
 
 echo Folders have been created
