6b66464e2feba3a6339e32ed3ad173f02ad00cd1
GlaIZier <kh.mv@bk.ru>
2019-03-11 06:52:32 +0300

    .travis.yml has been edited

--- folders.bat
+++ folders.bat
@@ -1,19 +1,18 @@
-set disk = %1
-if [%disk%] == [] (
-    set disk = "D"
-)
+echo Creating folders script is being executed
 
+set disk=%1
+if not defined disk set disk=D
 echo Disk is set to %disk%
 
-set folders = "%disk%:\google-drive %disk%:\movies %disk%:\tv-series %disk%:\downloads %disk%:\games"
+set folders=%disk%:\google-drive %disk%:\movies %disk%:\tv-series %disk%:\downloads %disk%:\games
 
 echo Folders to be created %folders%
 
 for %%f in (%folders%) do (
-    IF EXIST %f% (
-        mkdir %f%
-        echo %f% has been created
-    ) ELSE (
-        echo %f% already exists
+	if not exist %%f (
+        mkdir %%f
+        echo %%f has been created
+    ) else (
+        echo %%f already exists
     )
 )
